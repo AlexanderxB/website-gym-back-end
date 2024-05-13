474739d1b6494fa6014e2d85cafdc45425e4926e
Alexander <alepues1@gmail.com>
2024-05-13 16:48:18 -0500

    Se agrego el get en index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const app =require('./app');
 
+app.get("/", (req,res) => {
+    res.send("Hello world");
+});
+
 app.listen(app.get('port'), () => {
     console.log("Servidor escuchando en el puerto", app.get("port"));
 });
